22238eeeda0fb730ad74219d62258d5019f93d2e
David Slater <dslater@illinois.edu>
2021-10-27 15:29:56 -0500

    Added space in copyright

--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -5,7 +5,7 @@
                 <p class='text-center'>
                 <span class='text-muted'><strong><em>If you have any questions, please contact <?php echo EMAIL; ?>.</em></strong>
                 <br><em><a href='https://www.vpaa.uillinois.edu/resources/web_privacy' target='_blank'> University of Illinois System Web Privacy Notice</a></em>
-                <br><em>&copy;<?php echo date('Y'); ?> University of Illinois Board of Trustees</em></span>
+                <br><em>&copy; <?php echo date('Y'); ?> University of Illinois Board of Trustees</em></span>
                 </p>
         </div>
 
